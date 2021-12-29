d2ec71c4bceef5380beb25ff7efa829d9baf6ad6
Luca <clementluca6@gmail.com>
2021-12-29 19:01:19 -0300

    add script.js 'classes e atributos'

---
+++ classes-e-atributos/script.js
@@ -0,0 +1,18 @@
+/*
+Exercícios 
+
+#1 - Adicione a classe 'ativo' a todos os itens do menu.
+const menu = document.querySelectorAll('.menu-container ul li a');
+
+menu.classList.add('ativo')
+
+#2 - Remova a classe 'ativo' de todos os itens do menu e mantenha apenas no primeiro.
+
+#3 - Verifique se as imagens pososuem o atributo 'alt'
+
+#4 - Modifique o href do link externo do menu.
+
+*/
+
+const menu = document.querySelector('.menu-container ul li a');
+menu.classList.add('ativo')
